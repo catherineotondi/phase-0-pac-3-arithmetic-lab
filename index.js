429eca3e3c3d687f6fbf4de5748bbab7a05f6050
catherineotondi <catherineotondi@gmail.com>
2022-08-05 03:04:31 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,34 @@
+
+
+function add(a, b) {
+    return a + b;
+}
+ 
+function subtract(a, b) {
+    return a - b;
+ }
+function multiply(a,b) {
+    return a * b;
+ }
+function divide(a, b) {
+    return a / b;
+}
+ 
+function increment(a) {
+    return a+=1
+}
+
+function decrement(a) {
+    return a-=1
+}
+
+function makeInt(a) {
+    return parseInt(a.toString(), 10);
+}
+(makeInt('sldkjflksjf'))
+
+function preserveDecimal(string) {
+    return parseFloat(string)
+}
+preserveDecimal("2.222")
+preserveDecimal('sldkjflksjf')
